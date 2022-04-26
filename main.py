fe0b4b3b77d9ed231439a67c5bf1f5aa208a2fa6
yahya boyalı <yahyaboyali@gmail.com>
2022-04-26 11:44:04 +0300

    modifying specific pixels

--- main.py
+++ main.py
@@ -22,9 +22,16 @@ cv2.imshow("deneme",ilkresim)
 
 #print(ilkresim[(230,80)])#230 a 80 deki bgr değerini alıyor
 
-print("resmin boyutu: "+ str(ilkresim.size))
-print("resmin özellikleri: "+ str(ilkresim.shape))
+#print("resmin boyutu: "+ str(ilkresim.size))
+#print("resmin özellikleri: "+ str(ilkresim.shape))
 
-print("resmin veri tipi: "+ str(ilkresim.dtype))
+#print("resmin veri tipi: "+ str(ilkresim.dtype))
+
+# pixel renk değiştirme
+
+#ilkresim[112,45] = [255,255,255]
+#for i in range(100): # çizgi çizdi i boyunca
+ #   ilkresim[70,i]=[255,255,255]
+cv2.imshow("pixel degisti",ilkresim)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
